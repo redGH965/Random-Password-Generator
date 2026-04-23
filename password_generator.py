0c80c1e9a04826a860336b6e03c1bed2ca6cb8a3
redGH965 <redcrffd@gmail.com>
2026-04-23 09:16:12 +0500

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,19 @@
+import random
+import string
 
+def generate_password(length, use_digits, use_letters, use_special):
+    if length < 1:
+        raise ValueError("Длина пароля должна быть больше 0")
+
+    chars = ''
+    if use_digits:
+        chars += string.digits
+    if use_letters:
+        chars += string.ascii_letters
+    if use_special:
+        chars += string.punctuation
+
+    if not chars:
+        raise ValueError("Выберите хотя бы один тип символов")
+
+    return ''.join(random.choices(chars, k=length))
